c9830a9b1e26fc34277ddc8c551162ef23afb001
astuart.pett@gmail.com <astuart.pett@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2013-07-09 02:57:57 +0000

    BUGFIX/UPGRADE: Upgrading to 3.0
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/ecommerce_countries/trunk@4579 950b781c-c544-efab-2f99-90083ee69683

--- _config.php
+++ _config.php
@@ -1,3 +1,4 @@
 <?php
-
-//Object::add_extension('Product', 'ProductByCountrySTD');
+//===================---------------- START ecommerce_countries MODULE ----------------===================
+//Object::add_extension('ProductPage', 'ProductByCountrySTD');
+//===================---------------- END ecommerce_countries MODULE ----------------===================
