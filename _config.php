cdc4af9ab140a9ef8b6fc0604e7a367d12648a47
Nicolaas <github@sunnysideup.co.nz>
2016-09-03 21:22:00 +1200

    fixes

--- _config.php
+++ _config.php
@@ -3,3 +3,4 @@
 
 /* CONSIDER ADDING TO _config.php
 //CMSMenu::add_menu_item('distributors', 'World Pricing', 'distributors-admin', $controllerClass = null, $priority = 1, array('target' => '_blank'));
+*/
