8900472e306265c34b5182e9e35d29b9d4e02531
sunnysideup <sunnysideupdevs@gmail.com>
2017-01-03 13:07:06 +1300

    PATCH: module clean-up

--- tests/EcommerceCountriesTest.php
+++ tests/EcommerceCountriesTest.php
@@ -1,13 +1,12 @@
 <?php
-class EcommerceCountriesTest extends SapphireTest {
+class EcommerceCountriesTest extends SapphireTest
+{
+    protected $usesDatabase = false;
 
-	protected $usesDatabase = false;
+    protected $requiredExtensions = array();
 
-	protected $requiredExtensions = array();
-
-	public function testMyMethod() {
-		$this->assertEquals(1, 1);
-	}
-
+    public function testMyMethod()
+    {
+        $this->assertEquals(1, 1);
+    }
 }
-
