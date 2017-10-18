817dad11d02eef3c3774cc84db880b1ed93b2148
Nicolaas Thiemen Francken @ B&C <github@sunnysideup.co.nz>
2017-10-18 14:32:57 +1300

    PATCH: making HTTPS optional

--- code/control/CountryPrice_Page_Controller_Extension.php
+++ code/control/CountryPrice_Page_Controller_Extension.php
@@ -94,7 +94,7 @@ class CountryPrice_Page_Controller_Extension extends Extension
             return null;
         }
         //to do: add query here!
-        $protocol = Director::is_https() ? 'https://' : 'http://'
+        $protocol = Director::is_https() ? 'https://' : 'http://';
         $oldURL = $protocol.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
 
         $hasCountrySegment = CountryPrice_Translation::get_country_url_provider()->hasCountrySegment($oldURL);
